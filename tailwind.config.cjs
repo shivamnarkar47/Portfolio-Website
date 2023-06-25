ed559f20528e479ef298130352c9696380eb90af
shivamnarkar47 <shivamnarkar47@gmail.com>
2023-06-25 19:07:29 +0530

    Increased Speed 🚄

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,9 @@ module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
